092c47a57b7ed8f37f55d62328abc678832589f9
mehra <mehradotdev@gmail.com>
2025-12-12 08:54:48 +0530

    chore: add theming

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,29 +3,29 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className="flex-1 bg-white dark:bg-black items-center justify-center px-8">
+    <View className="flex-1 bg-background items-center justify-center px-8">
       {/* Heading */}
-      <Text className="text-4xl font-extrabold text-gray-800 dark:text-white mb-3 tracking-tight">
+      <Text className="text-4xl font-extrabold text-foreground mb-3 tracking-tight">
         🚀 Welcome
       </Text>
 
       {/* Subheading */}
-      <Text className="text-xl dark:text-white text-gray-700 mb-8 text-center leading-relaxed">
+      <Text className="text-xl text-foreground mb-8 text-center leading-relaxed">
         Build beautiful apps with{" "}
-        <Text className="text-blue-500 font-semibold">
+        <Text className="text-blue-400 font-semibold">
           Expo (Router) + Uniwind 🔥
         </Text>
       </Text>
 
       {/* Instruction text */}
-      <Text className="text-base text-gray-600 dark:text-white text-center max-w-sm">
+      <Text className="text-base text-foreground text-center max-w-sm">
         Start customizing your app by editing{" "}
-        <Text className="font-semibold text-gray-800 dark:text-white">
+        <Text className="font-semibold text-foreground">
           app/index.tsx
         </Text>
       </Text>
 
-      <StatusBar style="dark" />
+      <StatusBar style="auto" />
     </View>
   );
 }
