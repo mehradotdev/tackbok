0e32146c053219bb3230535d6e2fd5759dce7f4e
mehra <mehradotdev@gmail.com>
2025-12-13 11:58:59 +0530

    feat: add web support, fix error - Stack from expo-router not working

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,31 @@
 import "../global.css";
 
-import { Slot } from "expo-router";
+import { Text } from "react-native";
+import { Stack } from "expo-router";
+import { useCSSVariable } from "uniwind";
 
 export default function Layout() {
-  return <Slot />;
+  const [primaryColor, primaryForeground] = useCSSVariable([
+    '--color-primary',
+    '--color-primary-foreground',
+  ]);
+
+  return (
+    <Stack screenOptions={{ 
+      headerShown: true, 
+      headerTitleAlign: 'center', 
+      headerTintColor: primaryForeground as string, 
+      headerStyle: { backgroundColor: primaryColor as string },
+      headerTitleStyle: { fontWeight: 'bold' }
+    }}>
+      <Stack.Screen name="index" options={{
+         title: 'Tackbok',
+         headerRight: () => <Text>Menu</Text>,
+         }} />
+      <Stack.Screen name="settings" options={{ 
+        title: 'Settings',
+        headerShown: false,
+      }} />
+    </Stack>
+  );
 }
